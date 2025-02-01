1432dbf8e746a40899dfffba7da7b2bbd7f5c446
Fernnog <fernandocnogueira@gmail.com>
2025-02-01 09:19:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+import { inicializarOrcamentos } from './modules/orcamentos.js';
+import { inicializarPedidos } from './modules/pedidos.js';
+import { inicializarRelatorios } from './modules/relatorios.js';
+import { inicializarBackup } from './modules/backup.js';
+import { carregarDados, mostrarPagina, atualizarPainelUltimoBackup } from './modules/ui.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+    carregarDados();
+    inicializarOrcamentos();
+    inicializarPedidos();
+    inicializarRelatorios();
+    inicializarBackup();
+    mostrarPagina('form-orcamento');
+    atualizarPainelUltimoBackup();
+});
